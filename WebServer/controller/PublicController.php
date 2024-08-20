8ae8f1674f9438e5067f653c2ef6b979dcbd005b
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-08-20 17:08:08 +0700

    fix: fix UI

--- WebServer/controller/PublicController.php
+++ WebServer/controller/PublicController.php
@@ -4,9 +4,9 @@ class PublicController
     public function index()
     {
         $VIEW = "./view/Dashboard.phtml";
-        $VIEW = "./view/ProfileManagement/ListProfiles.phtml";
-        $VIEW = "./view/LeaveManagement/ListLeaveRequests.phtml";
-        $VIEW = "./view/WFHManagement/ListWFHRequests.phtml";
+        // $VIEW = "./view/ProfileManagement/ListProfiles.phtml";
+        // $VIEW = "./view/LeaveManagement/ListLeaveRequests.phtml";
+        // $VIEW = "./view/WFHManagement/ListWFHRequests.phtml";
         
         require("./template/Layout.phtml");
     }
