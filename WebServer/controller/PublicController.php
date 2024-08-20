163d53b5aa0c87ea316bb1889c806fdcb6f73d3f
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-08-20 21:53:11 +0700

    feat: add timesheet UI

--- WebServer/controller/PublicController.php
+++ WebServer/controller/PublicController.php
@@ -4,9 +4,13 @@ class PublicController
     public function index()
     {
         $VIEW = "./view/Dashboard.phtml";
+        
+        // $VIEW = "./view/LoginPage.phtml";
         // $VIEW = "./view/ProfileManagement/ListProfiles.phtml";
         // $VIEW = "./view/LeaveManagement/ListLeaveRequests.phtml";
         // $VIEW = "./view/WFHManagement/ListWFHRequests.phtml";
+        // $VIEW = "./view/TimesheetManagement/ListTimesheets.phtml";
+        $VIEW = "./view/TimesheetManagement/TimesheetDetail.phtml";
         
         require("./template/Layout.phtml");
     }
