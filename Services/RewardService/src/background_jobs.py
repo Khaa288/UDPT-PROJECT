8b1c32bb4008338db714dc6dd0a7e11e4aea676a
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-09-09 18:59:52 +0700

    feat: add monthly point logic

--- Services/RewardService/src/background_jobs.py
+++ Services/RewardService/src/background_jobs.py
@@ -7,7 +7,13 @@ class BackgroundConfig:
 
 scheduler = APScheduler()
 
-@scheduler.task('interval', id='add_monthly_points', seconds=10, misfire_grace_time=900)
+# Test background job run every 10 seconds
+@scheduler.task('interval', id='test_add_monthly_points', seconds=10, misfire_grace_time=900)
+def test_add_monthly_points_job():
+    print('Monthly point added')
+
+# background job run every last day of month
+@scheduler.task('interval', id='add_monthly_points', seconds=2592000, misfire_grace_time=900)
 def add_monthly_points_job():
     Reward.add_monthly_points()
     print('Monthly point added')
