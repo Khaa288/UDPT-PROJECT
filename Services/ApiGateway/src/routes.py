db4209ef6041a9fab482ffb50aec65cfd31ccfaf
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-09-09 15:55:11 +0700

    feat: integrate reward service to apigateway

--- Services/ApiGateway/src/routes.py
+++ Services/ApiGateway/src/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from src.controllers.profile_service_controller import auth, employee
 from src.controllers.request_management_service_controller import attendance, wfh, leave, timesheet
+from src.controllers.reward_service_controller import reward
 
 # main blueprint to be registered with application
 api = Blueprint('api', __name__)
@@ -14,3 +15,6 @@ api.register_blueprint(attendance, url_prefix="/attendance")
 api.register_blueprint(wfh, url_prefix="/wfh")
 api.register_blueprint(leave, url_prefix="/leave")
 api.register_blueprint(timesheet, url_prefix="/timesheet")
+
+# REWARD SERVICE API
+api.register_blueprint(reward, url_prefix="/reward")
